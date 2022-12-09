abf9f58b4d94d25c258c06605b238e6eaf175ed0
Johan Dahlbom <johan@dahlbom.eu>
2022-12-09 06:12:10 +0100

    update

--- scripts/securePostConfig.ps1
+++ scripts/securePostConfig.ps1
@@ -12,7 +12,7 @@ param (
     [ValidateSet("dev", "qa", "test", "uat", "prod")]
     $Environment = ''
 )
-
+# The sample script are not supported under any Microsoft standard support program or service. The sample codes are provided AS IS without warranty of any kind. Microsoft further disclaims all implied warranties including, without limitation, any implied warranties of merchantability or of fitness for a particular purpose. The entire risk arising out of the use or performance of the sample codes and documentation remains with you. In no event shall Microsoft, its authors, owners of this repository or anyone else involved in the creation, production, or delivery of the scripts be liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages
 begin {
     # Ensure correct Azure Context
     Set-AzContext -SubscriptionId $SubscriptionId -WarningAction Ignore
